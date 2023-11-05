a2624c2e76d91c0a8be69196a78729be12396177
Nguyen Xuan Truong <kiaisoft.truong.nguyen@gmail.com>
2023-11-05 21:56:11 +0700

    template engine and static file, scss

--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
-const express = require('express')
-const morgan = require('morgan')
-const path = require('path')
-const app = express()
-const port = 3000
-const handlebars = require("express-handlebars")
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const hbs = require("express-handlebars");
+const app = express();
+const port = 3000;
 
-app.use(morgan('combined'))
+//SET DEFAULT PATH
+app.use(express.static(path.join(__dirname, "public/")))
 
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'resources/views'));
-console.log(path.join(__dirname, 'resources/views'));
+// HTTP logger
+app.use(morgan("combined"));
 
-app.get('/', (req, res) => {
-  res.render('home');
+// Template engine
+app.engine(
+  "hbs",
+  hbs.engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
-// app.get('/', (req,  res) => {
-//   return res.send('Hello World!')
-// })
+app.get("/news", (req, res) => {
+  res.render("news");
+});
 
-
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
