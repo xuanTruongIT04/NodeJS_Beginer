e1ca04428457fdf62eff64135bb5fa69fa944313
Nguyen Xuan Truong <kiaisoft.truong.nguyen@gmail.com>
2023-11-06 11:28:00 +0700

    refactor code: create routes, controller files

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const hbs = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require("./routes/index.router");
+
 //SET DEFAULT PATH
 app.use(express.static(path.join(__dirname, "public/")));
 
@@ -29,23 +31,8 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
-
-app.get("/search", (req, res) => {
-  res.render("search");
-});
-
-app.post("/search", (req, res) => {
-  console.log(req.body);
-
-  res.send();
-});
+//Route init
+route(app);
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
---
+++ src/routes/index.router.js
@@ -0,0 +1,10 @@
+const newsRouter = require("./news.router");
+const siteRouter = require("./site.router");
+
+function route(app) {
+  app.use("/news", newsRouter);
+
+  app.use("/", siteRouter);
+}
+
+module.exports = route;
