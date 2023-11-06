ee973f5672a0d72e7130a443fc63267aa1ae3ffb
Nguyen Xuan Truong <kiaisoft.truong.nguyen@gmail.com>
2023-11-06 14:32:50 +0700

    install prittier lint-staged husky

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
-const path = require("path");
-const express = require("express");
-const morgan = require("morgan");
-const hbs = require("express-handlebars");
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const hbs = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-const route = require("./routes/index.router");
+const route = require('./routes/index.router');
 
 //SET DEFAULT PATH
-app.use(express.static(path.join(__dirname, "public/")));
+app.use(express.static(path.join(__dirname, 'public/')));
 
 // Middleware
 app.use(
-  express.urlencoded({
-    extened: true,
-  })
+    express.urlencoded({
+        extened: true,
+    }),
 );
 app.use(express.json());
 
 // HTTP logger
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 // Template engine
 app.engine(
-  "hbs",
-  hbs.engine({
-    extname: ".hbs",
-  })
+    'hbs',
+    hbs.engine({
+        extname: '.hbs',
+    }),
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 //Route init
 route(app);
 
 app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`),
 );
--- src/routes/site.router.js
+++ src/routes/site.router.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const siteController = require("../app/controllers/SiteController");
+const siteController = require('../app/controllers/SiteController');
 
-router.use("/search", siteController.search);
+router.use('/search', siteController.search);
 
-router.use("/", siteController.index);
+router.use('/', siteController.index);
 
 module.exports = router;
