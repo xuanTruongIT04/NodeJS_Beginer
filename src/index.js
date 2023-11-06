1a6e968150dc4b73077737e8bb68690b82aa6ccf
Nguyen Xuan Truong <kiaisoft.truong.nguyen@gmail.com>
2023-11-06 07:43:35 +0700

    query parameters

--- src/index.js
+++ src/index.js
@@ -29,6 +29,10 @@ app.get("/news", (req, res) => {
   res.render("news");
 });
 
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
